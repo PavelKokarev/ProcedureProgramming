eec53453ef90d5b63894aea89b7414921525209d
PavelKokarev <pavelkockareff2002@yandex.ru>
2021-11-30 00:15:27 +0300

    Задание 4-1 #7
    1. Исправлены ошибки.

--- Task 4-1/Task 4-1/main.cpp
+++ Task 4-1/Task 4-1/main.cpp
@@ -1,352 +1,188 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
-#include <string>
+#include <cmath>
 #include <random>
+#include <iomanip>
 
 using namespace std;
 
 /**
- * \brief Заполнение массива случайными числами.
- * \param size размер массива.
- * \param minValue минимальное значение элементов массива.
- * \param maxValue максимальное значение элементов массива.
- * \return заполненный массив.
- */
-int* FillRandomArray(size_t size, int minValue, int maxValue);
+* \brief Заполнение массива случайными числами
+* \param array Массив
+* \param size Размер массива
+* \param min Минимально возможное число
+* \param max Максимально возможное число
+**/
+void RandomArray(int* array, const size_t size, const int min, const int max);
 
 /**
- * \brief Создание нового массива, путем копирования исходного массива
- * и замены второго элемента на максимальный отрицательный элемент (если такой имеется)
- * \param array исходный массив.
- * \param size размер массива.
- * \return новый измененный массив.
- */
-int* MaxMinElementChanged(const int* array, const size_t size);
+* \brief Ввод массива с клавиатуры
+* \param array Массив
+* \param size Размер массива
+**/
+void UserArray(int* array, const size_t size);
 
 /**
- * \brief Вывод массива на консоль.
- * \param array массив.
- * \param size размер массива.
- * \param message претворяющая печать надпись.
- */
-int ArrayPrint(const int* array, size_t size, const string& message = "");
+* \brief Сумма элементов, по модулю меньших 10
+* \param array Массив
+* \param size Размер массива
+**/
+int SumNumbers(int* array, const size_t size);
 
 /**
- * \brief
- */
-enum class ArrayInputWay
+* \brief Количество элементов(значения которых больше значения последующего)
+* \param array Массив
+* \param size Размер массива
+* \param A значение по условию
+**/
+void NumberElements(int* array, const size_t size);
+
+/**
+* \brief Новый массив(все элементы, кратные 3, умножаются на 3 элемент массива)
+* \param array Массив
+* \param second_array Обработанный массив
+* \param size Размер массива
+**/
+void SecondArray(int* array, int* second_array, const size_t size);
+
+enum class ArrayType
 {
-    random,
-    keyboard
+	RandomNumber = 1,
+	UserNumber = 2
 };
-
-/**
- * \brief Нахождение суммы четных отрицательных значений элементов массива.
- * \param array массив.
- * \param size размер массива.
- * \return сумма четных отрицательных значений элементов массива.
- * Если массива не существует, то сумма = 42.
- */
-int SumOddValues(const int* array, size_t size);
-
-/**
- * \brief Метод, возвращающий заполненный пользователем массив.
- * \param size размер массива.
- * \param minValue минимальное значение элементов массива.
- * \param maxValue максимальное значение элементов массива.
- * \param message надпись для пользователя.
- * \return заполненный массив.
- */
-int* FillUserArray(size_t size, const string& message = "");
-
-/**
- * \brief Вывод на консоль всех индексов, элементы которых кратны 3 и 6.
- * \param size размер массива.
- * \param number заданный индекс.
- */
-int PrintIndexesAboveConst(int* array, const size_t size);
-
-/**
- * \brief Ввод целого числа.
- * \param message претворяющая печать надпись.
- * \return целое число, введенное пользователем.
- */
-int IntInput(const string& message);
-
-/**
- * \brief Определяет, четное и отрицательное ли число?
- * \param element число.
- * \return если четное отрицательное, то true. Если нечетное - false
- */
-bool IsOdd(int element);
-
-/*
-* \brief Поиск максимального элемента
-* \return Максимальный элемент
-*/
-int GetMaxElem(const int* array, const size_t size);
-
-/*
-* \brief Кратность шести
-*/
-bool IsOddSix(const int element);
-
-/*
-* \brief Крастность трём
-*/
-bool IsOddThree(const int element);
-
-/*
-* \brief Поиск минмиального элемента
-* \return Минимальный элемент
-*/
-int GetMinElem(const int* array, const size_t size);
-
-/**
- * \brief Метод, изменяющий неправильный диапазон.
- * \param min минимальное значение.
- * Будет изменено, если ошибочно было больше максимального.
- * Результатом станет -10.
- * \param max максимальное значение.
- * Будет изменено, если ошибочно было меньше минимального.
- * Результатом станет 10.
- */
-void ChangeWrongRange(int& min, int& max);
-
-/**
- * \brief Копирование исходного массива.
- * \param array исходный массив.
- * \param size размер массива.
- * \return новый массив со скопированными элементами.
- */
-int* ArrayCopy(const int* array, size_t size);
 
 int main()
 {
-    cout << "Input array size = ";
-    size_t size;
-    cin >> size;
-    cout << "Which way of array filling are you prefer?\n"
-        << static_cast<int>(ArrayInputWay::random) << " - random,\n"
-        << static_cast<int>(ArrayInputWay::keyboard) << " - keyboard.\n"
-        << "Your choice is ";
-    int choice;
-    std::cin >> choice;
+	setlocale(LC_ALL, "ru");
 
-    try
-    {
-        const auto chosen = static_cast<ArrayInputWay>(choice);
-        int* myArray = nullptr;
+	cout.precision(3);
+	cout << fixed;
 
-        switch (chosen)
-        {
-        case ArrayInputWay::random:
-        {
-            auto minValue = IntInput("Input min value of array = ");
-            auto maxValue = IntInput("Input max value of array = ");
-            if (maxValue <= minValue)
-            {
-                ChangeWrongRange(minValue, maxValue);
-            }
-            myArray = FillRandomArray(size, minValue, maxValue);
-            break;
-        }
-        case ArrayInputWay::keyboard:
-        {
-            myArray = FillUserArray(size, "Input elements of the array");
-            break;
-        }
-        default:
-            cout << "You entered wrong choice!\n";
-            return 1;
-        }
+	const int MIN = -40, MAX = 40;
+	int sum;
 
-        ArrayPrint(myArray, size, "\nThe array is:\n");
+	cout << "Please input array length: ";
+	size_t size;
+	cin >> size;
 
-        const auto sum = SumOddValues(myArray, size);
-        cout << "Sum of elements odd value = " << sum << "\n";
+	int* array = new int[size];
+	int* second_array = new int[size];
 
-        PrintIndexesAboveConst(myArray, size);
+	cout << "\nChoose array:\n" << "1) Array with random number\n" << "2) Array with user number\n" << "1 or 2: ";
+	int choice;
+	cin >> choice;
 
-        const auto changedArray = MaxMinElementChanged(myArray, size);
-        ArrayPrint(changedArray, size, "\nThe changed array is:\n");
+	const auto arraytype = static_cast<ArrayType>(choice);
 
-        delete[] myArray;
-        delete[] changedArray;
+	switch (arraytype)
+	{
+	case ArrayType::RandomNumber:
+	{
+		RandomArray(array, size, MIN, MAX);
+		break;
+	}
+	case ArrayType::UserNumber:
+	{
+		UserArray(array, size);
+		break;
+	}
+	default:
+		break;
+	}
 
-        cout << endl;
-        return 0;
-    }
-    catch (out_of_range&)
-    {
-        cout << "You entered wrong choice!";
-        return 1;
-    }
+	cout << "Сумма чётных отрицательных элементов " << SumNumbers(array, size) << endl;
 
+	cout << "Индексы элементов, значения которых кратны 3 и 6: \n";
+
+	NumberElements(array, size);
+	cout << endl;
+	cout << "Новый массив, в котором элементы, находящиеся между максимальным и минимальным, заменяются нулями: " << endl; SecondArray(array, second_array, size);
+
+	delete[] array;
+	delete[] second_array;
 }
 
-int* FillRandomArray(const size_t size, const int minValue, const int maxValue)
+void RandomArray(int* array, const size_t size, const int min, const int max)
 {
-    //Will be used to obtain a seed for the random number engine
-    std::random_device rd;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	const uniform_int_distribution<> uniformIntDistribution(min, max);
 
-    //Standard mersenne_twister_engine seeded with rd()
-    std::mt19937 gen(rd());
-
-    const std::uniform_int_distribution<> uniformIntDistribution(minValue, maxValue);
-
-    auto* array = new int[size];
-    for (size_t index = 0; index < size; index++)
-    {
-        array[index] = uniformIntDistribution(gen);
-    }
-
-    return array;
+	for (size_t i = 0; i < size; ++i)
+		array[i] = uniformIntDistribution(gen);
+	for (size_t i = 0; i < size; ++i)
+	{
+		cout << "array[" << i << "] = " << array[i] << endl;
+	}
 }
 
-int* FillUserArray(const size_t size, const string& message)
+void UserArray(int* array, const size_t size)
 {
-    auto* array = new int[size];
-    cout << message << "\n";
-    for (size_t index = 0; index < size; index++)
-    {
-        cin >> array[index];
-    }
-    return array;
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << "array[" << i << "] = ";
+		cin >> array[i];
+	}
 }
 
-int ArrayPrint(const int* array, const size_t size, const string& message)
+int SumNumbers(int* array, const size_t size)
 {
-    if (array == nullptr)
-    {
-        cout << "The array doesn't exist";
-        return 0;
-    }
-
-    cout << message;
-    for (size_t index = 0; index < size; index++)
-    {
-        cout << array[index] << " ";
-    }
-    cout << "\n";
+	int sum = 0;
+	for (size_t i = 0; i < size; ++i)
+	{
+		if (array[i] < 0 && array[i] % 2 == 0)
+		{
+			sum += array[i];
+		}
+	}
+	return sum;
 }
 
-void ChangeWrongRange(int& min, int& max)
+void NumberElements(int* array, const size_t size)
 {
-    min = -1000;
-    max = 1000;
-    cout << "\nYou input wrong range. Right range is ["
-        << min << ","
-        << max << "]\n";
+	bool k = false;
+	for (size_t i = 0; i < size; i++) {
+		if (array[i] % 3 == 0 && array[i] % 6 == 0) {
+			cout << "Номер элемента: " << i << endl;
+			k = true;
+		}
+	}
+	if (k == false) {
+		cout << "Таких элементов нет ";
+	}
 }
 
-int IntInput(const string& message)
+void SecondArray(int* array, int* second_array, const size_t size)
 {
-    cout << message;
-    int temp;
-    cin >> temp;
-    return temp;
-}
+	second_array = array;
+	double null = 0;
+	int max = INT_MIN;
+	int min = INT_MAX;
+	size_t max_index = size, min_index = 0;
 
-int PrintIndexesAboveConst(int* array, const size_t size)
-{
-    for (size_t i = 0; i < size; i++)
-        if (IsOddSix(array[i]))
-            return i;
-}
+	for (size_t i = 0; i < size; i++) {
+		if (array[i] > max) {
+			max_index = i;
+			max = array[i];
+		}
+	}
+	for (size_t i = 0; i < size; i++) {
+		if (array[i] < min) {
+			min_index = i;
+			min = array[i];
+		}
+	}
 
-int SumOddValues(const int* array, const size_t size)
-{
-    if (array == nullptr)
-        return 42;
+	if (max_index < min_index) {
+		for (size_t i = max_index + 1; i < min_index; i++)
+			second_array[i] = null;
+	}
+	else {
+		for (size_t i = min_index + 1; i < max_index; i++)
+			second_array[i] = null;
+	}
 
-    auto sum = 0;
-    for (size_t index = 0; index < size; index++)
-    {
-        if (IsOdd(array[index]) && array[index] < 0)
-            sum += array[index];
-    }
-    return sum;
-}
-
-bool IsOdd(const int element)
-{
-    return element % 2 != 1;
-}
-
-bool IsOddSix(const int element)
-{
-    return element % 6 != 0;
-}
-
-bool IsOddThree(const int element)
-{
-    return element % 3 != 0;
-}
-
-int* MaxMinElementChanged(const int* array, const size_t size)
-{
-    if (array == nullptr)
-        return nullptr;
-
-    const auto null = 0;
-    const auto newArray = ArrayCopy(array, size);
-    const auto maxElem = GetMaxElem(array, size);
-    const auto minElem = GetMinElem(array, size);
-
-    for (size_t i = 0; i < size; i++)
-        if (array[i] == minElem)
-        {
-            size_t indexFirst = i;
-            for (size_t index = indexFirst + 1; index < size; index++)
-            {
-                do {
-                    newArray[index] = null;
-                } while (array[index] < maxElem);
-            }
-        }
-        else if (array[i] == maxElem)
-        {
-            size_t indexFirst = i;
-            for (size_t index = indexFirst + 1; index < size; index++)
-            {
-                do {
-                    newArray[index] = null;
-                } while (array[index] > minElem);
-            }
-        }
-}
-
-int GetMaxElem(const int* array, const size_t size)
-{
-    auto maximum = -1001;
-    for (unsigned int i = 0; i < size; i++)
-    {
-        if (array[i] > maximum)
-            maximum = array[i];
-    }
-    return maximum;
-}
-
-int GetMinElem(const int* array, const size_t size)
-{
-    auto minimum = 1001;
-    for (unsigned int i = 0; i < size; i++)
-    {
-        if (array[i] < minimum)
-            minimum = array[i];
-    }
-    return minimum;
-}
-
-int* ArrayCopy(const int* array, const size_t size)
-{
-    if (array == nullptr)
-        return nullptr;
-
-    const auto target = new int[size];
-    for (size_t index = 0; index < size; index++)
-    {
-        target[index] = array[index];
-    }
-    return target;
+	for (size_t i = 0; i < size; i++)
+		cout << second_array[i] << " ";
 }
